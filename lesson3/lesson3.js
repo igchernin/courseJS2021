94deba7d8417348d1d81c81f45609357f524c5b1
igchernin <chernin91@gmail.com>
2021-11-15 21:15:41 +0200

    обновил файлы

---
+++ lesson3/lesson3.js
@@ -0,0 +1,51 @@
+
+//цикл , перебор чисел
+// debugger
+// for (let i = 0; i <= 100; i++){
+//     document.write('<div>item x</div>')
+// }
+
+
+//цикл, перебор чисел и вывести каждый раз ее резльтат
+// for (let i = 0; i <= 100; i++){
+//     document.write(`<div>day ${i}</div>`)
+// }
+
+//цикл, перебор чисел и
+// let array = [11,22,33]
+// for (let i = 0; i < 3; i++){
+//     document.write(array[i])
+// }
+
+//вывесли типо страницы
+// let array = ['page11','page22','page33']
+// for (let i = 0; i < 3; i++){
+//     document.write(`<div>${array[i]}</div>`)
+// }
+
+//вывесли типо страницы
+let array = ['page11','page22','page33']
+for (let i = 0; i < 3; i++){
+    document.write(`<div>${array[i]}</div>`)
+}
+
+//как быстро построить перебор
+//array.fori //и нажать таб
+//или
+//itar
+
+//цикл в обратном порядке
+//ritar
+
+
+//pages.forof
+//iter
+// let pages = ['page1','page2','page3','page4']
+// for (let 0 of pages){
+//     document.write(`<div>`)
+//
+//     document.write(`</div>`)
+//
+// }
+
+
