3220b98ef171dfbd5c953ac50659bdd6ddb03635
igchernin <chernin91@gmail.com>
2021-11-16 22:57:02 +0200

    обновил файлы

--- lesson3/lesson3-cw.js
+++ lesson3/lesson3-cw.js
@@ -1,9 +1,60 @@
 // зробити масив з 10 чисел [2,17,13,6,22,31,45,66,100,-18]та:
 //     1. перебрати його циклом while
+
+let array = [2,17,13,6,22,31,45,66,100,-18]
+
+//
+// let i = 0
+// while (i < array.length) {
+//     console.log(array[i])
+//     i++;
+// }
+
+
+
 //     2. перебрати його циклом for
+// for (i = 0; i < array.length; i++) {
+//     document.write(`${array[i]} <br>`)
+//     console.log(array[i])
+// }
+
+
+
 //     3. перебрати циклом while та вивести  числа тільки з непарним індексом
+
+// let i = 0
+// while (i < array.length) {
+//         console.log(array[i]);
+//     i+=2
+// }
+
 // 4. перебрати циклом for та вивести  числа тільки з непарним індексом
+
+// for (let i = 0; i < array.length; i+=2) {
+//     const arrayElement = array[i];
+//     console.log(arrayElement);
+// }
+
+// for (let i = 0; i < array.length; i++) {
+//     if (i % 2 === 0) {
+//         const arrayElement = array[i];
+//         console.log(arrayElement);
+//     }
+// }
+
+
 // 5. перебрати циклом while та вивести  числа тільки парні  значення
+
+let i = 0
+while (i < array.length) {
+    if (array[i] % 2 === 0) {
+        console.log(array[i])
+    }
+    i++;
+}
+
+
+
 // 6. перебрати циклом for та вивести  числа тільки парні  значення
 // 7. замінити кожне число кратне 3 на слово "okten"
 // 8. вивести масив в зворотньому порядку.
